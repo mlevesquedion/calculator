ba9a15a5ff94994675de3b4b9fe856236a6af0af
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-01-09 14:58:24 -0500

    Rewrite test

--- test_lexer.py
+++ test_lexer.py
@@ -1,33 +1,22 @@
-from quicktest import test
-from lexer import Lexer
-from token import Integer, Operator, Lparen, Rparen
+from calculator.lexer import Lexer
+from calculator.lexeme import Integer, Operator, Lparen, Rparen
 
 
-def lex(text):
-    return list(Lexer(text))
-
-
-if __name__ == '__main__':
-    test(
-        lex,
-        [
-            [
-                '     2    -   (8 + 9)    *  3   /  2 ^   5',
-                [
-                    Integer(2),
-                    Operator('-'),
-                    Lparen(),
-                    Integer('8'),
-                    Operator('+'),
-                    Integer('9'),
-                    Rparen(),
-                    Operator('*'),
-                    Integer('3'),
-                    Operator('/'),
-                    Integer('2'),
-                    Operator('^'),
-                    Integer('5')
-                ]
-            ]
-        ]
-    )
+def test_lexer():
+    text = '     2    -   (8 + 9)    *  3   /  2 ^   5'
+    expected = [
+        Integer(2),
+        Operator('-'),
+        Lparen(),
+        Integer('8'),
+        Operator('+'),
+        Integer('9'),
+        Rparen(),
+        Operator('*'),
+        Integer('3'),
+        Operator('/'),
+        Integer('2'),
+        Operator('^'),
+        Integer('5')
+    ]
+    assert list(Lexer(text)) == expected
